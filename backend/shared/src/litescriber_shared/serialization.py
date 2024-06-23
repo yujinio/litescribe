d9008a08deba0bfc0ed3a19518ae9e6a1be32104
yujinio <root@yujin.io>
2024-06-23 14:55:12 +0200

    add shared serialization/deserialization

---
+++ backend/shared/src/litescriber_shared/serialization.py
@@ -0,0 +1,31 @@
+from dataclasses import asdict
+
+import msgpack
+
+from litescriber_shared.dto import TranscripionResult, TranscriptionRequest
+
+
+def serialize_transcription_request(request: TranscriptionRequest) -> bytes:
+    return serialize_dict(asdict(request))
+
+
+def deserialize_transcription_request(data: bytes) -> TranscriptionRequest:
+    unpacked_data = deserialize_dict(data)
+    return TranscriptionRequest(**unpacked_data)
+
+
+def serialize_transcription_result(result: TranscripionResult) -> bytes:
+    return serialize_dict(asdict(result))
+
+
+def deserialize_transcription_result(data: bytes) -> TranscripionResult:
+    unpacked_data = deserialize_dict(data)
+    return TranscripionResult(**unpacked_data)
+
+
+def serialize_dict(data: dict) -> bytes:
+    return msgpack.packb(data)
+
+
+def deserialize_dict(data: bytes) -> dict:
+    return msgpack.unpackb(data)
